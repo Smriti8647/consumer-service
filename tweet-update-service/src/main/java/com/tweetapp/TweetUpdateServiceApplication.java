63b841d71f2b2314fe7def9c6303795482be52ec
Smriti <63850511+Smriti8647@users.noreply.github.com>
2022-05-12 13:49:02 +0530

    Added consumer sevice

---
+++ tweet-update-service/src/main/java/com/tweetapp/TweetUpdateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tweetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetUpdateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetUpdateServiceApplication.class, args);
+	}
+
+}
